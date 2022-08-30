ab93e48371cb7dca49d18ccd8bbc06ff23e8e3c9
Larissa Dinart <lari_dinart@hotmail.com>
2022-08-30 13:01:03 -0300

    Menu inicial alterado

--- PBanco_Morangao/Program.cs
+++ PBanco_Morangao/Program.cs
@@ -7,7 +7,7 @@ namespace PBanco_Morangao
         static void Main(string[] args)
         {
             MenuInicial();
-           
+
         }
 
         static public void MenuInicial()
@@ -17,25 +17,47 @@ namespace PBanco_Morangao
             Console.WriteLine("Bem vindo ao banco Morangão!");
             Console.WriteLine("\nDigite a agência desejada: \n\n1 - Morango Do Nordeste\n2 - Torta De Morango \n");
             agencia = int.Parse(Console.ReadLine());
-            if(agencia == 1)
+            Console.WriteLine();
+            if (agencia == 1)
             {
                 Console.WriteLine("Agência escolhida: Morango Do Nordeste.");
-                Console.WriteLine("\nEscolha a opção desejada: \n\n1-Cadastrar cliente\n2-Já Sou Cliente");
+                Console.WriteLine("\nEscolha a opção desejada: \n\n1-Cadastrar cliente\n2-Já Sou Cliente\n3-Voltar Ao Menu Anterior");
                 opcao = int.Parse(Console.ReadLine());
-                if(opcao == 1)
+                Console.WriteLine();
+                if (opcao == 1)
                 {
-                    Console.WriteLine("Opção: Cadastrar Cliente");
+                    Console.WriteLine("Opção: Cadastrar Cliente.");
+                    //CadastraCliente();
                 }
-            }else if(agencia == 2)
+                else if (opcao == 2)
+                {
+                    Console.WriteLine("Opção: Já Sou Cliente.");
+                }
+                else if (opcao == 3)
+                {
+                    MenuInicial();
+                }
+                else
+                {
+                    Console.WriteLine("\nOpção inválida, tente novamente!\n");
+                }
+            }
+            else if (agencia == 2)
             {
                 Console.WriteLine("Agência escolhida: Torta De Morango.");
-                Console.WriteLine("\nEscolha a opção desejada: \n\n1-Já sou cliente\n2-Cadastrar Cliente");
+                Console.WriteLine("\nEscolha a opção desejada: \n\n1-Já sou cliente\n2-Cadastrar Cliente\n3-Voltar Ao Menu Anterior");
                 opcao = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+            else if (agencia == 3)
+            {
+                MenuInicial();
             }
             else
             {
                 Console.WriteLine("\nOpção inválida, tente novamente!\n");
                 MenuInicial();
+               
             }
         }
     }
