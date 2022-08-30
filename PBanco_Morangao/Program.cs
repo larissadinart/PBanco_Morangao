08b72b72b0492eb4ea50c29c6ca1d8ba2879b9a7
Larissa Dinart <lari_dinart@hotmail.com>
2022-08-30 12:11:17 -0300

    Criação da função Menu

--- PBanco_Morangao/Program.cs
+++ PBanco_Morangao/Program.cs
@@ -6,10 +6,37 @@ namespace PBanco_Morangao
     {
         static void Main(string[] args)
         {
+            MenuInicial();
+           
+        }
 
-            Pessoa pessoa = new Pessoa();
-
-    
+        static public void MenuInicial()
+        {
+            int agencia;
+            int opcao;
+            Console.WriteLine("Bem vindo ao banco Morangão!");
+            Console.WriteLine("\nDigite a agência desejada: \n\n1 - Morango Do Nordeste\n2 - Torta De Morango \n");
+            agencia = int.Parse(Console.ReadLine());
+            if(agencia == 1)
+            {
+                Console.WriteLine("Agência escolhida: Morango Do Nordeste.");
+                Console.WriteLine("\nEscolha a opção desejada: \n\n1-Cadastrar cliente\n2-Já Sou Cliente");
+                opcao = int.Parse(Console.ReadLine());
+                if(opcao == 1)
+                {
+                    Console.WriteLine("Opção: Cadastrar Cliente");
+                }
+            }else if(agencia == 2)
+            {
+                Console.WriteLine("Agência escolhida: Torta De Morango.");
+                Console.WriteLine("\nEscolha a opção desejada: \n\n1-Já sou cliente\n2-Cadastrar Cliente");
+                opcao = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("\nOpção inválida, tente novamente!\n");
+                MenuInicial();
+            }
         }
     }
 }
