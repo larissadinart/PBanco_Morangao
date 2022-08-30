42bea5993a1bb313a1a41379e30bf5e7ed1fdd98
Larissa Dinart <lari_dinart@hotmail.com>
2022-08-30 10:33:32 -0300

    Criação da classe Pessoa.

---
+++ PBanco_Morangao/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBanco_Morangao
+{
+    internal class Pessoa
+    {
+        private String Nome;
+        private String Telefone;
+        private String Email;
+        private String Cpf;
+        private String Genero;
+
+        public Pessoa()
+        {
+            Console.WriteLine("Digite o nome: ");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Digite o telefone: ");
+            Telefone = Console.ReadLine();
+            Console.WriteLine("Digite o e-mail: ");
+            Email = Console.ReadLine();
+            Console.WriteLine("Digite o cpf: ");
+            Cpf = Console.ReadLine();
+            Console.WriteLine("Digite o gênero: \nM-Masculino \nF-Feminino");
+
+        }
+        public void LerPessoa()
+        {
+            Console.WriteLine($"Nome: {Nome}\n Telefone: {Telefone} \n E-mail: {Email} \n Cpf: {Cpf} \n Gênero:{Genero}");
+        }
+    }
+
+
+}
